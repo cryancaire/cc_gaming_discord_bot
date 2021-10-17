4821c5c610521afa12693155cb1aa1067718fe59
Corey Caire <cryancaire@gmail.com>
2021-10-17 18:32:15 -0500

    Another fix

--- commands/music.js
+++ commands/music.js
@@ -1,16 +1,16 @@
 exports.run = (client, message, args) => {
 	var VC = message.member.voiceChannel;
     var whatSong = args[1];
-    
+    var dispatcher;
     if (!VC)
         return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
 		VC.join()
         .then(connection => {
             if (whatSong) {
-                const dispatcher = connection.playFile('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+                dispatcher = connection.playFile('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
                 dispatcher.on("end", end => {VC.leave()});
             } else {
-                const dispatcher = connection.playFile(whatSong);
+                dispatcher = connection.playFile(whatSong);
                 dispatcher.on("end", end => {VC.leave()});
             }
             
