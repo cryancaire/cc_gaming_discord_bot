d9c3fc463dbf4c1547f01a443b4a3ff9627d6f2e
Corey Caire <cryancaire@gmail.com>
2021-10-17 18:27:19 -0500

    Updated music command

--- commands/music.js
+++ commands/music.js
@@ -1,10 +1,16 @@
 exports.run = (client, message, args) => {
 	var VC = message.member.voiceChannel;
+    var whatSong = args[1];
+    const dispatcher;
     if (!VC)
         return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
 		VC.join()
         .then(connection => {
-            const dispatcher = connection.playFile('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+            if (whatSong) {
+                dispatcher = connection.playFile('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+            } else {
+                dispatcher = connection.playFile(whatSong);
+            }
             dispatcher.on("end", end => {VC.leave()});
         })
         .catch(console.error);
