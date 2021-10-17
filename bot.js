4212fb3ba8ea6912f5a49eca10743705dac5f3ad
Corey Caire <cryancaire@gmail.com>
2021-10-17 17:51:15 -0500

    Further removed dependency from config.json

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 
-const config = require("./conf.json");
 	//assignable roles via reactions
 	//   role name 	  role id 				emoji id/name
 	var theAssignableRoles = [
